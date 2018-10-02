737d6bb9a1bd8b20ad9a3d38d4495eb695b136cf
hariprstya <hari.prstya@gmail.com>
2018-10-02 15:09:53 +0700

    python write file

---
+++ PythonFiles/write_file/write_file.py
@@ -0,0 +1,8 @@
+file = open('result.txt','w')
+
+file.write('Hello World!\n')
+file.write('This is our new text file.\n')
+file.write('We can create a new line. ')
+file.write('Cool, isnt it?!')
+
+file.close()
